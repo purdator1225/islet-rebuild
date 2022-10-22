db1e5ecc6fef08eb2f98433ac8ed21b560aef461
purdator1225 <briansh1225@gmail.com>
2022-10-23 02:42:07 +0800

    attempted sass

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
+import { Routes, Route } from "react-router-dom";
 
-import { Routes, Route } from 'react-router-dom';
+import Navbar from "./routes/navbar/navbar.component";
 
-import Navbar from './routes/navbar/navbar.component';
-
-import Home from './routes/home/home.component';
+import Home from "./routes/home/home.component";
 
 function App() {
-
-  const videos = [{
-    id: 744082385, 
-    name: "Lumiere",
-    role: "Full Production" ,
-    key:1
-  },{
-    id: 696325791, 
-    name: "Batman",
-    role: "Full Production" ,
-    key:2
-  },
-  {id: 606285014, 
-    name: "Ode to my GrandMothers",
-    role: "Full Production",
-    key:3}
-]
-
-
+  const videos = [
+    {
+      id: 744082385,
+      name: "Lumiere",
+      role: "Full Production",
+      key: 1,
+    },
+    {
+      id: 696325791,
+      name: "Batman",
+      role: "Full Production",
+      key: 2,
+    },
+    {
+      id: 606285014,
+      name: "Ode to my GrandMothers",
+      role: "Full Production",
+      key: 3,
+    },
+  ];
 
   return (
-
     <Routes>
-    <Route path="/" element={<Navbar/>}>
-
-    <Route index element={<Home videos={videos}/>}/>
-
-    </Route>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Home videos={videos} />} />
+      </Route>
     </Routes>
   );
 }
